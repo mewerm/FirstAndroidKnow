019e9af1c729c36a992845ad86ea68045609ddc8
Maxim <25-nv-86@mail.ru>
2022-06-21 23:10:14 +0300

    Вроде все условия is done

--- app/src/main/java/com/maximmesh/androidknow/MainActivity.java
+++ app/src/main/java/com/maximmesh/androidknow/MainActivity.java
@@ -1,16 +1,23 @@
 package com.maximmesh.androidknow;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.prefs.Preferences;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.second_activity);
 
-        //Проверка гита
 
     }
 }
