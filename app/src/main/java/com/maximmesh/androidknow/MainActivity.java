7689e0609829cb912458f7c7b8c3d926c4844354
Maxim <25-nv-86@mail.ru>
2022-06-22 17:06:24 +0300

    Надеюсь это последний коммит первой домашки)

--- app/src/main/java/com/maximmesh/androidknow/MainActivity.java
+++ app/src/main/java/com/maximmesh/androidknow/MainActivity.java
@@ -1,23 +1,28 @@
 package com.maximmesh.androidknow;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
-import java.util.prefs.Preferences;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_activity);
+        setContentView(R.layout.activity_main);
 
+        Button button = findViewById(R.id.button_save);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent switcher = new Intent(MainActivity.this, SecondActivity.class);
+                MainActivity.this.startActivity(switcher);
+            }
+        });
 
     }
 }
